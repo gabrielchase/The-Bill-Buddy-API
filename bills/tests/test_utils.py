8258ce0433b872e2551b528f8b8c033e3af97911
gabrielchase <gchase.patron@gmail.com>
2018-01-15 22:14:34 +0800

    Test for `handle_service_instance`

---
+++ bills/tests/test_utils.py
@@ -0,0 +1,25 @@
+from bills.utils import handle_service_instance
+from bills.models import Service
+
+import pytest
+pytestmark = pytest.mark.django_db
+
+
+class TestBillsUtils:
+
+    def test_handle_service_instance(self):
+        school_service_1 = handle_service_instance('school')
+        assert school_service_1.name == 'School'
+        assert school_service_1.id 
+        assert Service.objects.count() == 1
+
+        school_service_2 = handle_service_instance('ScHooL')
+        print(Service.objects.all())
+        assert school_service_2.name == 'School'
+        assert school_service_2.id == school_service_1.id 
+        assert Service.objects.count() == 1
+
+        work_service = handle_service_instance('WORK')
+        assert work_service.name == 'Work'
+        assert work_service.id 
+        assert Service.objects.count() == 2
