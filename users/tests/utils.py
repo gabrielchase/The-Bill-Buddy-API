c1114fbccaa68ae1c9b77da1dc8466aa7a617ac4
gabrielchase <gchase.patron@gmail.com>
2018-01-17 14:23:13 +0800

    Function to get a jwt to put for HTTP_AUTHORIZATION header

--- users/tests/utils.py
+++ users/tests/utils.py
@@ -1,3 +1,4 @@
+from rest_framework_jwt.settings import api_settings
 from django.contrib.auth import get_user_model
 
 import random
@@ -8,3 +9,15 @@ User = get_user_model()
 def generate_random_password():
     N = random.randint(7, 30)
     return ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=N))
+
+def get_jwt_header(email):
+    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+    
+    user = User.objects.get(email=email)    
+    
+    if user:
+        payload = jwt_payload_handler(user)
+        return 'JWT {}'.format(jwt_encode_handler(payload))
+    else:
+        return False
