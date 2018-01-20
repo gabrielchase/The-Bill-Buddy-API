a9bbb64c46deb4037242544376088a1358b4c0c2
gabrielchase <gchase.patron@gmail.com>
2018-01-20 23:22:56 +0800

    Implement payment create in serializers

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework import status
 
+from bills.models import Bill
 from payments.models import Payment
 
 import json
@@ -10,7 +13,32 @@ User = get_user_model()
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    bill_id = serializers.IntegerField()
 
     class Meta:
         model = Payment
-        fields = ('id', 'amount', 'date', 'status', 'additional_notes', 'bill', 'user')
+        fields = ('id', 'amount', 'due_date', 'date_paid', 'status', 'additional_notes', 'bill_id')
+
+    def create(self, data):
+        print('Creating new payment with data: ', data)
+        
+        try:
+            bill_id = data.get('bill_id')
+            print(bill_id)
+            bill_instance = Bill.objects.get(id=bill_id)
+            print(bill_instance)
+            new_payment = Payment.objects.create(
+                amount=data.get('amount'),
+                due_date=data.get('due_date'),
+                date_paid=data.get('date_paid'),
+                status=data.get('status'),
+                additional_notes=data.get('additional_notes'),
+                bill=bill_instance,
+                user=self.context['request'].user
+            )
+        except Bill.DoesNotExist:
+            raise ValueError('Bill with id {} does not exist'.format(bill_id))
+
+        print('New bill created: ', new_payment)
+        
+        return new_payment
