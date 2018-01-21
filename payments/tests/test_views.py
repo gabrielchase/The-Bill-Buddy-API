136fd8e494dd7e3f504f85f8026d154402ac8474
gabrielchase <gchase.patron@gmail.com>
2018-01-21 23:05:32 +0800

    Create tests for Payment GET list and retrieve requests

--- payments/tests/test_views.py
+++ payments/tests/test_views.py
@@ -1,7 +1,9 @@
+from mixer.backend.django import mixer
+
 from rest_framework.test import APIRequestFactory
 
 from payments.models import Payment
-from payments.tests.fixtures import new_payment_info
+from payments.tests.fixtures import (new_payment, other_payment, new_payment_info, get_status)
 from payments.views import PaymentViewSet
 
 from bills.tests.fixtures import new_bill
@@ -44,3 +46,76 @@ class TestPaymentsViews:
         
         assert payment_instance.bill == new_bill
         assert payment_instance.user == new_bill.user
+
+    def test_payment_list(self, new_bill):
+        Payment.objects.create(
+            amount=mixer.faker.positive_decimal(),
+            due_date=mixer.faker.date(),
+            date_paid=mixer.faker.date(),
+            status=get_status(),
+            additional_notes=mixer.faker.text(),
+            bill=new_bill,
+            user=new_bill.user
+        )
+
+        Payment.objects.create(
+            amount=mixer.faker.positive_decimal(),
+            due_date=mixer.faker.date(),
+            date_paid=mixer.faker.date(),
+            status=get_status(),
+            additional_notes=mixer.faker.text(),
+            bill=new_bill,
+            user=new_bill.user
+        )
+
+        Payment.objects.create(
+            amount=mixer.faker.positive_decimal(),
+            due_date=mixer.faker.date(),
+            date_paid=mixer.faker.date(),
+            status=get_status(),
+            additional_notes=mixer.faker.text(),
+            bill=new_bill,
+            user=new_bill.user
+        )
+
+        view = PaymentViewSet.as_view({'get': 'list'})
+        request = factory.get(
+            PAYMENTS_URI,
+            HTTP_AUTHORIZATION=get_jwt_header(new_bill.user.email)
+        )
+        response = view(request)
+
+        assert response.status_code == 200
+        assert len(response.data) == 3
+        for payment in response.data:
+            assert payment.get('id')
+            assert payment.get('bill_id') == new_bill.id
+            assert payment.get('user_id') == new_bill.user.id 
+
+    def test_retrieve_good(self, new_payment):
+        view = PaymentViewSet.as_view({'get': 'retrieve'})
+        request = factory.get(
+            PAYMENTS_URI,
+            HTTP_AUTHORIZATION=get_jwt_header(new_payment.user.email)
+        )
+        response = view(request, pk=new_payment.id)
+
+        assert response.status_code == 200
+        assert response.data.get('id') == new_payment.id
+        assert response.data.get('amount') == str(round(new_payment.amount, 2))
+        assert response.data.get('due_date') == new_payment.due_date
+        assert response.data.get('date_paid') == new_payment.date_paid
+        assert response.data.get('status') == new_payment.status
+        assert response.data.get('additional_notes') == new_payment.additional_notes
+        assert response.data.get('bill_id') == new_payment.bill.id
+        assert response.data.get('user_id') == new_payment.bill.user.id 
+
+    def test_retrieve_bad(self, new_payment, other_user):
+        view = PaymentViewSet.as_view({'get': 'retrieve'})
+        request = factory.get(
+            PAYMENTS_URI,
+            HTTP_AUTHORIZATION=get_jwt_header(other_user.email)
+        )
+        response = view(request, pk=new_payment.id)
+
+        assert response.status_code == 403
