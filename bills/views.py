e0f6066a5ef4c41640d7f657934016fdb31424f5
gabrielchase <gchase.patron@gmail.com>
2018-01-23 16:31:21 +0800

    Display `BillDetailSerializer` on 'retrieve' and create `ServiceList` view

--- bills/views.py
+++ bills/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import (generics, viewsets)
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-from bills.models import Bill
-from bills.permissions import BillPermission
-from bills.serializers import BillSerializer
+from bills.models import (Bill, Service)
+from bills.permissions import (BillPermission, ServicePermission)
+from bills.serializers import (BillSerializer, BillDetailSerializer, ServiceSerializer)
 
 
 class BillViewSet(viewsets.ModelViewSet):
@@ -25,4 +25,21 @@ class BillViewSet(viewsets.ModelViewSet):
             bills = bills.filter(service__name=service_name.title())
 
         return bills
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return BillDetailSerializer
+        else:
+            return BillSerializer
         
+
+class ServiceList(generics.ListAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (ServicePermission,)
+
+    def get_queryset(self):
+        user_bills = self.request.user.bills.all()
+        user_services = [bill.service for bill in user_bills]
+        return set(user_services)
--- thebillbuddy/urls.py
+++ thebillbuddy/urls.py
@@ -5,13 +5,14 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_jwt.views import verify_jwt_token
 
 from users.views import (UserViewSet, LoginAPIView)
-from bills.views import BillViewSet
+from bills.views import (BillViewSet, ServiceList)
 from payments.views import PaymentViewSet
 
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
 router.register('bills', BillViewSet)
+# router.register('services', ServiceViewSet)
 router.register('payments', PaymentViewSet)
 
 
@@ -19,4 +20,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/login/', LoginAPIView.as_view()),
     path('api/token-verify/', verify_jwt_token),
+
+    path('api/services/', ServiceList.as_view())
 ]
