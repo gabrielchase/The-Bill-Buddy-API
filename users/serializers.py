3df97154ba68d36b5707e71b9b2b3fbe1d2a6aff
gabrielchase <gchase.patron@gmail.com>
2018-02-14 16:12:20 +0800

    Get paid payments with a due date equal to this year

--- users/serializers.py
+++ users/serializers.py
@@ -80,7 +80,7 @@ class UserSerializer(serializers.ModelSerializer):
         expenditure_this_year = { 'total': 0 }
         payments = Payment.objects.filter(
             user=user_instance, 
-            date_paid__year=TODAY.year, # not required but more explicit
+            due_date__year=TODAY.year, # not required but more explicit
             status='Paid'
         )
         keys = []
